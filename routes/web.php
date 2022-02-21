6686ca517ed1581f6287c4d5d656cd55f91e9162
Meilina Anggraeni Putri <meilinaanggraeniputri@gmail.com>
2022-02-21 07:41:22 -0800

    broken-link

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,35 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/pricing', function () {
+    return view('pricing');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/blog-grids', function () {
+    return view('bloggrids');
+});
+
+Route::get('/blog-details', function () {
+    return view('blogdetails');
+});
+
+Route::get('/signup', function () {
+    return view('signup');
+});
+
+Route::get('/signin', function () {
+    return view('signin');
+});
+
+Route::get('/404', function () {
+    return view('404');
+});
